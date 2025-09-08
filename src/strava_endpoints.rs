f2c346b59e661af602dee87d73c66d77e8387701
gonzalogacc <gonzalogacc@gmail.com>
2025-09-08 16:41:18 +0200

    Change error handling

--- src/strava_endpoints.rs
+++ src/strava_endpoints.rs
@@ -60,8 +60,11 @@ async fn code_exchange_handler(
     let sc =
         StravaClient::init("https://www.strava.com", &state.strava_client_secret);
 
-    let token_set = sc.code_exchange(&code_params.code).await;
-    reqwest_response_handling(token_set)
+    let token_set = match sc.code_exchange(&code_params.code).await {
+        Ok(tokens) => tokens,
+        Err(e) => return Err(reqwest_response_handling(e))
+    };
+    Ok(ApiResponse::JsonData(token_set))
 }
 
 async fn token_refresh_handler(
@@ -69,47 +72,51 @@ async fn token_refresh_handler(
 ) -> Result<ApiResponse<TokenSet>, ApiError> {
     let sc =
         StravaClient::init("https://www.strava.com", &state.strava_client_secret);
-    let token_set = sc.refresh_token().await;
-    reqwest_response_handling(token_set)
+
+    let token_set = match sc.refresh_token().await {
+        Ok(tokens) => tokens,
+        Err(e) => return Err(reqwest_response_handling(e))
+    };
+
+    Ok(ApiResponse::JsonData(token_set))
 }
 
-fn reqwest_response_handling<T>(
-    result: Result<T, reqwest::Error>,
-) -> Result<ApiResponse<T>, ApiError> {
-    match result {
-        Ok(r) => Ok(ApiResponse::JsonData(r)),
-        Err(e) => {
-            println!("{:?}", e);
-            match e.status() {
-                Some(StatusCode::UNAUTHORIZED) => Err(ApiError {
-                    status_code: StatusCode::UNAUTHORIZED,
-                    message: "Something went wrong".to_string(),
-                }),
-                _ => Err(ApiError {
-                    status_code: StatusCode::INTERNAL_SERVER_ERROR,
-                    message: "Internal server error :D".to_string(),
-                }),
-            }
-        },
-    }
+fn reqwest_response_handling(
+    error: reqwest::Error,
+) -> ApiError {
+        match error.status() {
+            Some(StatusCode::UNAUTHORIZED) => ApiError {
+                status_code: StatusCode::UNAUTHORIZED,
+                message: "Something went wrong".to_string(),
+            },
+            _ => ApiError {
+                status_code: StatusCode::INTERNAL_SERVER_ERROR,
+                message: "Internal server error :D".to_string(),
+            },
+        }
 }
 
 async fn me_handler(State(state): State<Arc<AppState>>) -> Result<ApiResponse<Athlete>, ApiError> {
     let sc =
         StravaClient::init("https://www.strava.com", &state.strava_client_secret);
-    let me = sc.get_user().await;
-    reqwest_response_handling(me)
+    let me = match sc.get_user().await {
+        Ok(me) => me,
+        Err(e) => return Err(reqwest_response_handling(e)),
+    };
+
+    Ok(ApiResponse::JsonData(me))
 }
 
 async fn activity_handler(
     State(state): State<Arc<AppState>>
 ) -> Result<ApiResponse<Vec<Activity>>, ApiError> {
     let sc = StravaClient::init("https://www.strava.com", &state.strava_client_secret);
-    let activities = sc.get_activities().await;
+    let activities = match sc.get_activities().await {
+        Ok(act) => act,
+        Err(e) => return Err(reqwest_response_handling(e))
+    };
 
     // Write activities to a file
-    let _ = sc.write_activities(&activities.as_ref().unwrap(), "./activities_file.json").await.unwrap();
-
-    let act = reqwest_response_handling(activities);
-    act
+    let _ = sc.write_activities(&activities.as_ref(), "./activities_file.json").await.unwrap();
+    Ok(ApiResponse::JsonData(activities))
 }
